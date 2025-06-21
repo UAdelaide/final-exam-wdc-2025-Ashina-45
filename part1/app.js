819db784bf9c5f3089f5f2f3e56bcda64c398ebd
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 16:52:04 +0930

    Final Commit

--- part1/app.js
+++ part1/app.js
@@ -16,6 +16,7 @@ const pool = mysql.createPool({
 });
 
 // Sample insert data function (run once on startup)
+async function insertSampleData() {
   try {
     const conn = await pool.getConnection();
 
